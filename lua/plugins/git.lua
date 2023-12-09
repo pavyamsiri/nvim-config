2f7abb2ed19ab90916d9e94d678ff4b23f4a4a29
Pavadol Yamsiri <pavyamsiri@gmail.com>
2023-12-09 13:48:13 +1100

    Properly name all gitsign keymaps

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -58,46 +58,46 @@ return {
                 -- Stage hunk selected in visual mode
                 map("v", "<leader>hs", function()
                     gs.stage_hunk { vim.fn.line ".", vim.fin.line "v" }
-                end, { desc = "stage hunk (Git)" })
+                end, { desc = "[h]unk stage (Git)" })
 
                 -- Reset hunk selected in visual mode
                 map("v", "<leader>hr", function()
                     gs.reset_hunk { vim.fn.line ".", vim.fn.line "v" }
-                end, { desc = "Reset hunk (Git)" })
+                end, { desc = "[h]unk [r]eset (Git)" })
 
                 -- Normal mode
                 -- Stage hunk
-                map("n", "<leader>hs", gs.stage_hunk, { desc = "Stage hunk (Git)" })
+                map("n", "<leader>hs", gs.stage_hunk, { desc = "[h]unk [s]tage (Git)" })
                 -- Reset hunk
-                map("n", "<leader>hr", gs.reset_hunk, { desc = "Reset hunk (Git)" })
+                map("n", "<leader>hr", gs.reset_hunk, { desc = "[h]unk [r]eset (Git)" })
                 -- Stage buffer
-                map("n", "<leader>hS", gs.stage_buffer, { desc = "Stage buffer (Git)" })
+                map("n", "<leader>hS", gs.stage_buffer, { desc = "buffer [h]unk [S]tage (Git)" })
                 -- Undo stage hunk
-                map("n", "<leader>hu", gs.undo_stage_hunk, { desc = "Undo stage hunk (Git)" })
+                map("n", "<leader>hu", gs.undo_stage_hunk, { desc = "buffer [h]unk [u]ndo stage (Git)" })
                 -- Reset buffer
-                map("n", "<leader>hR", gs.reset_buffer, { desc = "Stage hunk (Git)" })
+                map("n", "<leader>hR", gs.reset_buffer, { desc = "buffer [h]unk [R]eset (Git)" })
                 -- Preview hunk
-                map("n", "<leader>hp", gs.preview_hunk, { desc = "Stage hunk (Git)" })
+                map("n", "<leader>hp", gs.preview_hunk, { desc = "[h]unk [p]review (Git)" })
                 -- Git blame
                 map("n", "<leader>hb", function()
                     gs.blame_line { full = false }
-                end, { desc = "Blame line (Git)" })
+                end, { desc = "[h]unk [b]lame (Git)" })
                 -- Git diff
-                map("n", "<leader>hd", gs.diffthis, { desc = "Diff line against index (Git)" })
+                map("n", "<leader>hd", gs.diffthis, { desc = "line [h]unk [d]iff vs. index (Git)" })
                 -- Git diff against last commit
                 map("n", "<leader>hD", function()
                     gs.diffthis "~"
-                end, { desc = "Diff line against last commit (Git)" })
+                end, { desc = "line [h]unk [D]iff vs. last commit (Git)" })
 
                 -- Toggles
                 -- Toggle current line blame
-                map("n", "<leader>tb", gs.toggle_current_line_blame, { desc = "Toggle current line blame (Git)" })
+                map("n", "<leader>tb", gs.toggle_current_line_blame, { desc = "line [t]oggle [b]lame (Git)" })
                 -- Toggle show deleted
-                map("n", "<leader>td", gs.toggle_deleted, { desc = "Toggle show deleted (Git)" })
+                map("n", "<leader>td", gs.toggle_deleted, { desc = "[t]oggle [d]eleted (Git)" })
 
                 -- Text object
                 -- NOTE: Not sure what this does
-                map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "Select hunk (Git)" })
+                map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "select [i] [h]unk (Git)" })
             end,
         }
     },
